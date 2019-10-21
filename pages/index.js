b73172db3cfedf895d23d2410cda3e031002cb5c
Fernando Henriques <fernando.trabalhos@gmail.com>
2019-10-20 21:46:27 -0300

    Initial setup

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Index = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+
+      <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Hic eaque explicabo inventore cum totam? Eligendi iure a odit, fugit reiciendis natus voluptate dolorum aliquid vel veniam quos, cupiditate, aperiam dignissimos?</p>
+    </div>
+  );
+};
+
+export default Index;
