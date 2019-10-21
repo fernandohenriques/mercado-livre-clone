407e3343d57727888cf4d38a13b32b1e81b17322
Fernando Henriques <fernando.trabalhos@gmail.com>
2019-10-20 21:48:12 -0300

    Add .next in gitignore

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "396d4ebfb01300064b48";
+/******/ 	var hotCurrentHash = "d05245f13eb8819b20c6";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
