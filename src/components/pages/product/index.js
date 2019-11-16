609b890d35e8422720c018e884a72e8f135b74cb
Fernando Henriques <fernando.trabalhos@gmail.com>
2019-11-16 00:20:46 -0300

    Finished product details

--- src/components/pages/product/index.js
+++ src/components/pages/product/index.js
@@ -20,36 +20,44 @@ const Product = ({ loading, product, categories }) => {
 
   return (
     <Page title={`Mercado Livre - ${product ? product.title : 'Produto não encontrado'}`}>
-      <section className={styles.container}>
+      <div className={styles.container}>
         {loading ? <Loading /> : (
           <>
             <Breadcrumbs categories={categories} />
             <SuperCard className={styles.product}>
               {product ? (
                 <>
-                  <div className={styles.cover}>
-                    <img src={product.picture} alt={product.title} title={product.title} />
-                  </div>
-                  <div className={styles.data}>
-                    <div className={styles.condition}>
-                      {`${conditions.get(product.condition)}${hasSoldQuantity ? ` - ${product.sold_quantity} vendidos` : ''}`}
+                  <section className={styles.coverAndData}>
+                    <div className={styles.cover}>
+                      <img src={product.picture} alt={product.title} title={product.title} />
                     </div>
-                    <div className={styles.name}>
-                      <strong>{product.title}</strong>
+                    <div className={styles.data}>
+                      <div className={styles.condition}>
+                        {`${conditions.get(product.condition)}${hasSoldQuantity ? ` - ${product.sold_quantity} vendidos` : ''}`}
+                      </div>
+                      <div className={styles.name}>
+                        <strong>{product.title}</strong>
+                      </div>
+                      <div className={styles.price}>
+                        <strong>{`$ ${product.price.amount.toFixed(2)}`}</strong>
+                      </div>
+                      <Button onClick={() => null}>
+                        Comprar
+                      </Button>
                     </div>
-                    <div className={styles.price}>
-                      <strong>{`$ ${product.price.amount.toFixed(2)}`}</strong>
-                    </div>
-                    <Button onClick={() => null}>
-                      Comprar
-                    </Button>
-                  </div>
+                  </section>
+                  {product.description && (
+                    <section className={styles.description}>
+                      <span>Descripción del producto</span>
+                      <p>{product.description}</p>
+                    </section>
+                  )}
                 </>
               ) : <h3>Produto não encontrado.</h3>}
             </SuperCard>
           </>
         )}
-      </section>
+      </div>
     </Page>
   );
 };
